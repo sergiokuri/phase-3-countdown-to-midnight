58be6ac7ec78e11a56a67a02a946045fb8d140e6
Sergio <kurisergio777777@gmail.com>
2022-05-20 12:11:44 -0400

    commit tree f34056085d44405135746507c11e4eba9a3cacfb

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
+require "pry"
 
-def countdown
+def countdown (number)
+    while number > 0 
+      puts  "#{number} SECOND(S)!"
+      sleep 1
+    number -=1
+    end 
+    puts "HAPPY NEW YEAR!"
+    "HAPPY NEW YEAR!"
 end
+
+countdown(10)
